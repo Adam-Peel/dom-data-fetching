54c57a825fa166d68dbd27ff6d0d13e38abee16c
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-13 11:26:14 +0100

    Retrieve API and diusplay

--- public/index.js
+++ public/index.js
@@ -1,1 +1,53 @@
-console.log("Hello Matt!");
+const loadUsersButton = document.getElementById("load-users");
+
+const resultList = document.getElementById("results-list");
+
+function getUsers(event) {
+  event.preventDefault();
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((body) =>
+      body.forEach((user) => {
+        const { name } = user;
+        const userItem = document.createElement("li");
+        userItem.classList.add("user-card");
+        const userName = document.createElement("h2");
+        userName.classList.add("username");
+        userName.textContent = name;
+
+        userItem.append(userName);
+        resultList.append(userItem);
+      })
+    );
+}
+
+loadUsersButton.addEventListener("click", getUsers);
+
+/*
+
+address
+: 
+{street: 'Kulas Light', suite: 'Apt. 556', city: 'Gwenborough', zipcode: '92998-3874', geo: {…}}
+company
+: 
+{name: 'Romaguera-Crona', catchPhrase: 'Multi-layered client-server neural-net', bs: 'harness real-time e-markets'}
+email
+: 
+"Sincere@april.biz"
+id
+: 
+1
+name
+: 
+"Leanne Graham"
+phone
+: 
+"1-770-736-8031 x56442"
+username
+: 
+"Bret"
+website
+: 
+"hildegard.org"
+
+*/
