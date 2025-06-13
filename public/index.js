34e61cbcba0976f27d1330000a91e36381a1a9a8
parrais <github@parrais.co.uk>
2025-06-13 12:05:58 +0100

    Add basic error handling functionality

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 const loadUsersButton = document.getElementById("load-users");
-
 const resultList = document.getElementById("results-list");
+const statusMessage = document.getElementById("status-message");
 
 function getUsers(event) {
   event.preventDefault();
   fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.status);
+      return response.json();
+    })
     .then((body) =>
       body.forEach((user) => {
         const { name } = user;
@@ -18,12 +21,26 @@ function getUsers(event) {
         userItem.append(userName);
         resultList.append(userItem);
       })
-    );
+    )
+    .catch((err) => {
+      statusMessage.innerHTML = err;
+      statusMessage.classList.add("error-message");
+    });
 }
 
 loadUsersButton.addEventListener("click", getUsers);
 
 /*
+loading...
+
+no response (network issue etc.)
+no users in response
+
+
+
+
+
+
 
 address
 : 
